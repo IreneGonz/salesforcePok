9e6da4a909b0e18797ae3c5246b57ad096fdb34e
IreneGonz <irenegonzalezagu@gmail.com>
2020-02-26 10:32:29 +0100

    Signed-off-by: IreneGonz <irenegonzalezagu@gmail.com>

--- pokemon/force-app/main/default/aura/index2/index2Controller.js
+++ pokemon/force-app/main/default/aura/index2/index2Controller.js
@@ -5,6 +5,18 @@
     component.set("v.msgEvento", mensaje);
   },
 
+  callAura: function(component, event, helper) {
+    var action = component.get("c.llamarAura");
+    //var action2 = component.get("c.llamarAura2");
+    action.setParams({
+      mensaje: "Por favor funciona"
+    });
+
+    component.set("v.myvariable", userInput + response.getReturnValue());
+
+    $A.enqueueAction(action);
+  },
+
   callControllerJs: function(component, event, helper) {
     console.log(event);
     console.log(component);
